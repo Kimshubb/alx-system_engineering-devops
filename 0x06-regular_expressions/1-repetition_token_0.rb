65fa9377203afec0b4c02770c8b4c0e81c24658e
Kimshubb <issakimanifmk@gmail.com>
2023-11-22 00:05:50 -0800

    repetite

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,20 +1,2 @@
 #!/usr/bin/env ruby
-
-# Check if the argument matches the regular expression
-def match_expression(argument)
-  regex = /hbt({2,5})n/
-  if argument.match?(regex)
-    puts argument.scan(regex).join
-  else
-    puts ''
-  end
-end
-
-# Check if an argument is provided
-if ARGV.empty?
-  puts 'Usage: ./match_expression.rb <string>'
-else
-  # Call the matching method with the provided argument
-  match_expression(ARGV[0])
-end
-
+puts ARGV[0].scan(/hbt{2,5}n/).join
