4ad1f6e29694f5d264360ad83debf21d4d7ebd66
Kimshubb <issakimanifmk@gmail.com>
2024-02-19 09:50:18 -0800

    missing coma

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -5,7 +5,7 @@ package { 'python3-pip':
 }
 
 exec {'install_flask':
-  command => /usr/bin/pip3 install Flask==2.1.0
+  command => /usr/bin/pip3 install Flask==2.1.0,
   path    => ['/bin', '/usr/bin'],
   unless  => '/usr/bin/pip3 show Flask | grep -q "^Version: 2.1.0$"',
 }
