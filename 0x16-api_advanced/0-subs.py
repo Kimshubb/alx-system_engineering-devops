a2013efd2a742669ae5d2fd36005bacc5d074bb7
Kimshubb <issakimanifmk@gmail.com>
2024-07-18 01:27:52 -0700

    how many subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+
+"""This is `0-subs.py` module. It contains one function
+`number_of_subscribers`
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
+    """This is a function that  queries the Reddit API and returns
+    the number of subscribers (not active users, total subscribers)
+    for a given subreddit
+    """
+
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-            "User-Agent": "ubuntu:Python")
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
-        return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+    headers = {"User-Agent": "ubuntu:Python"}
+
+    r = requests.get(url, headers=headers, allow_redirects=False)
+
+    if r.ok:
+        value = r.json().get("data").get("subscribers")
+        return value if value is not None else 0
+    return 0
